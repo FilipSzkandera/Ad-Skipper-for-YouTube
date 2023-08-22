e7d6b8b1e37f44cf7b43ccfaf0ecc4f042a4d807
FilipSzkandera <filip.szkandera@gmail.com>
2023-08-23 00:59:37 +0200

    Initial Commit

---
+++ Ad Skipper for YT/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Ad Skipper for YT
+//
+//  Created by Filip Szkandera on 23.08.2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
